7c674b4649abcbaa11555508c3f66c5e2f90b0e9
sebacerizza-dot <sebacerizza@gmail.com>
2026-06-14 21:16:58 -0300

    se agrega funcion correspondiente a opcion 2

--- utils.py
+++ utils.py
@@ -31,7 +31,7 @@ def pedir_texto(mensaje):
 def agregar_pais(ARCHIVO_CSV, nuevo_pais):
     try:
         #solicitamos el pais a agregar
-        print ("\nagregar pais:\n")
+        print ("\n----- AGREGAR PAIS -----\n")
         pais_a_agregar = pedir_texto("Ingrese el nombre del pais a agregar: ")
         #lo pasamos a minusculas y eliminamos cualquier espacio o indentacion que haya quedado
         nuevo_pais = pais_a_agregar.lower().strip()
@@ -44,7 +44,7 @@ def agregar_pais(ARCHIVO_CSV, nuevo_pais):
                 for fila in lector_dict:
                 # Comparamos directamente usando el nombre de la columna
                     if fila['nombre'].lower().strip() == nuevo_pais:
-                        print(f"El país '{nuevo_pais}' ya existe en el archivo. Operación cancelada.")
+                        print(f"El país '{nuevo_pais.title()}' ya existe en el archivo. Operación cancelada.")
                         return False
     
             # Si no existe, pide la informacion para agregarlo
@@ -108,3 +108,71 @@ def agregar_pais(ARCHIVO_CSV, nuevo_pais):
         print (f"Error inesperado: {e}")
 
 #opcion 2
+def actualizar_datos(ARCHIVO_CSV, nuevos_datos):
+
+    try:
+        print("\n----- MODIFICAR PAIS ----- \n")
+        #solicitamos el nombre del pais a buscar
+        pais_a_buscar = pedir_texto ("Ingrese el nombre del pais que desea actualizar los datos: ").strip().lower()
+        #vamos a generar un nuevo dict para poder cargar los datos actualizados
+        datos_actualizados = []
+        #ponemos las columnas como estan en el csv
+        columnas = ['nombre', 'poblacion', 'superficie', 'continente']
+        #creamo una bandera para saber si encontramos el pais
+        encontrado = False
+
+        #Abrimos el archivo en modo lectura
+        with open(ARCHIVO_CSV, mode='r', encoding='utf-8') as archivo:
+            lector_dict = csv.DictReader(archivo)
+            #recorremos todo el archivo
+            for fila in lector_dict:
+                #comparamos el nombre de la fila con el nombre ingresado
+                if fila ['nombre'].lower().strip() == pais_a_buscar:
+                    #cambiamos la bandera
+                    encontrado = True
+                    #mostramos los datos
+                    print (f"Pais {pais_a_buscar.title()} encontrado:")
+                    print ("Datos almacenados en la base de datos")
+                    print (f"Población: {fila['poblacion']} | Superficie: {fila['superficie']} | Continente: {fila['continente']}")
+
+                    #solicitamos los nuevos datos
+                    nueva_pob = pedir_int ("\nIngrese la cantidad de habitantes actualizada: ")
+                    nueva_sup = pedir_int ("Ingrese la superficie actualizada en km*2: ")
+
+                    #reemplazamos los datos en la fila actual
+                    fila['poblacion'] = nueva_pob 
+                    fila['superficie'] = nueva_sup 
+
+                #guardamos los datos en la nueva base de datos
+                datos_actualizados.append(fila)
+        
+        #revisamos si se encontro el pais
+        if not encontrado:
+            print (f"\nEl pais {pais_a_buscar.title()} no se encuentra en la base de datos. si desea ingresarlo utilice la opcion 1.\n")
+            return False
+        
+        #si encontramos el pais sobreescribimos los datos modificados al csv
+        elif encontrado:
+            with open(ARCHIVO_CSV, mode='w', newline='', encoding='utf-8') as archivo:
+                escritor_dict = csv.DictWriter(archivo, fieldnames=columnas)
+                #Escribimos las cabeceras de nuevo
+                escritor_dict.writeheader()  
+                # Escribimos todas las filas juntas
+                escritor_dict.writerows(datos_actualizados)
+            
+            #damos aviso de que realizo la operacion correctamente
+            print ("Datos actualizados correctamente.\n")
+            return True
+
+    #Si bien ya estan validadas las funciones donde se ingresan los datos. Se deja el except para mostrar como se usa
+    except ValueError:
+        print ("Error. ingrese un numero entero")
+    #se crea la validacion por si no se encuentra el archivo CSV
+    except FileNotFoundError:
+        print (f"El archivo {ARCHIVO_CSV} no se encuentro en la carpeta")
+    #verificamos que se pueda trabajar con el csv
+    except PermissionError:
+        print ("Error. Asegurese que el CSV no se encuentre abierto por otro programa.")
+    #Para cualquier otro error lo imprimimos por pantalla
+    except Exception as e:
+        print (f"Error inesperado: {e}")
